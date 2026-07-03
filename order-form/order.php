290c634bd063f22fa53a0a9c4bf654e99e830a95
Wejdan Hakami <wejdan.hakami.ha@gmail.com>
2026-07-03 22:57:13 +0300

    Add files via upload

---
+++ order-form/order.php
@@ -0,0 +1,10 @@
+<?php
+    $pen = $_POST['PEN'] ;
+    $Book = $_POST['BOOK'] ;
+    $pencil = $_POST['pencil'];
+	
+	echo "<h2>Order Details</h2>";
+    echo "Pen Quantity: " .  $pen . " . "<br>";
+    echo "Book Quantity: " . $Book . "  . "<br>";
+    echo "Pencil Quantity: " . $pencil . "  . "<br>";
+	?>
